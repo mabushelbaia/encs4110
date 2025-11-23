db76103cca6d99b952b544bbcbffffbc9756a5d9
Mohammad Abu-Shelbaia <mabushelbaia@gmail.com>
2025-11-23 20:59:48 +0200

    Update documentation for ADC and LCD modules
    - Corrected figure references and formatting in ENCS4110_07.pdf.
    - Added detailed ADC module overview, configuration steps, and example code in ENCS4110_08.pdf.
    - Included information on GPIO registers and their configuration for ADC input.
    - Enhanced examples for single ADC channel reading and internal temperature sensor usage.

--- docs/snippets/adc/example1.c
+++ docs/snippets/adc/example1.c
@@ -1,45 +1,35 @@
 #include "TM4C123.h"
-#include <stdio.h>
-
+#define GREEN_LED 1 << 3
 volatile unsigned int adc_value;
-
 int main(void)
 {
-
-    /* Enable Clock to ADC0 and GPIO pins*/
-    SYSCTL->RCGCGPIO |= (1 << 4); /* Enable Clock to GPIOE or PE3/AN0 */
-    SYSCTL->RCGCADC |= (1 << 0);  /* AD0 clock enable*/
-
-    /* initialize PE3 for AIN0 input  */
-    GPIOE->AFSEL |= (1 << 3); /* enable alternate function */
-    GPIOE->DEN &= ~(1 << 3);  /* disable digital function */
-    GPIOE->AMSEL |= (1 << 3); /* enable analog function */
-
-    /* initialize sample sequencer3 */
-    ADC0->ACTSS &= ~(1 << 3);            /* disable SS3 during configuration */
-    ADC0->EMUX &= ~0xF000;               /* software trigger conversion */
-    ADC0->SSMUX3 = 0;                    /* get input from channel 0 */
-    ADC0->SSCTL3 |= (1 << 1) | (1 << 2); /* take one sample at a time, set flag at 1st sample */
-    ADC0->ACTSS |= (1 << 3);             /* enable ADC0 sequencer 3 */
-
-    /*Iniitialize PF3 as a digital output pin */
+    SYSCTL->RCGCGPIO |= (1 << 4); // Enable Clock to GPIOE or PE3/AN0
+    SYSCTL->RCGCADC |= (1 << 0);  // AD0 clock enable
+    
+    GPIOE->AFSEL |= (1 << 3);  // enable alternate function
+    GPIOE->DEN &= ~(1 << 3);   // disable digital function
+    GPIOE->AMSEL |= (1 << 3);  // enable analog function
+    
+    ADC0->ACTSS &= ~(1 << 3);            // disable SS3 during configuration
+    ADC0->EMUX &= ~0xF000;                // Software Trigger
+    ADC0->SSMUX3 = 0;                    // get input from channel 0
+    ADC0->SSCTL3 |= (1 << 1) | (1 << 2); // take one sample at a time, set flag at 1st sample
+    ADC0->ACTSS |= (1 << 3);             // enable ADC0 sequencer 3
 
     SYSCTL->RCGCGPIO |= 0x20; // turn on bus clock for GPIOF
-    GPIOF->DIR |= 0x08;       // set GREEN pin as a digital output pin
-    GPIOF->DEN |= 0x08;       // Enable PF3 pin as a digital pin
+    GPIOF->DIR |= GREEN_LED;       // set GREEN pin as a digital output pin
+    GPIOF->DEN |= GREEN_LED;       // Enable PF3 pin as a digital pin
 
     while (1)
     {
-
-        ADC0->PSSI |= (1 << 3); /* Enable SS3 conversion or start sampling data from AN0 */
-        while ((ADC0->RIS & 8) == 0)
-            ;                      /* Wait untill sample conversion completed*/
-        adc_value = ADC0->SSFIFO3; /* read adc coversion result from SS3 FIFO*/
-        ADC0->ISC = 8;             /* clear coversion clear flag bit*/
+        ADC0->PSSI |= (1 << 3);            // initiate SS3 conversion
+        while ((ADC0->RIS & 8) == 0);      // Wait untill sample conversion completed
+        adc_value = ADC0->SSFIFO3;         // read adc coversion result from SS3 FIFO
+        ADC0->ISC = 8;                     // clear coversion clear flag bit
 
         if (adc_value >= 2048)
-            GPIOF->DATA = 0x08; /* turn on green LED*/
+            GPIOF->DATA |= GREEN_LED; // turn on green LED
         else if (adc_value < 2048)
-            GPIOF->DATA = 0x00; /* turn off green LED*/
+            GPIOF->DATA &= ~GREEN_LED; // turn off green LED
     }
 }
