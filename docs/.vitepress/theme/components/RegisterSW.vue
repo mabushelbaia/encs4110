6420c870d9cf61f8dec2179c64062f29ede6ce69
Mohammad Abu-Shelbaia <mabushelbaia@gmail.com>
2026-07-24 16:44:54 +0300

    refactor: simplify service worker registration and remove unused refresh logic

--- docs/.vitepress/theme/components/RegisterSW.vue
+++ docs/.vitepress/theme/components/RegisterSW.vue
@@ -2,27 +2,18 @@
 import { onBeforeMount, ref } from 'vue'
 
 const offlineReady = ref(false)
-const needRefresh = ref(false)
-
-let updateServiceWorker: (() => Promise<void>) | undefined
-
 function onOfflineReady() {
   offlineReady.value = true
 }
-function onNeedRefresh() {
-  needRefresh.value = true
-}
 async function close() {
   offlineReady.value = false
-  needRefresh.value = false
 }
 
 onBeforeMount(async () => {
   const { registerSW } = await import('virtual:pwa-register')
-  updateServiceWorker = registerSW({
+  registerSW({
     immediate: true,
     onOfflineReady,
-    onNeedRefresh,
     onRegistered() {
       console.info('Service Worker registered')
     },
@@ -34,23 +25,15 @@ onBeforeMount(async () => {
 </script>
 
 <template>
-  <template v-if="offlineReady || needRefresh">
+  <template v-if="offlineReady">
     <div
       class="pwa-toast"
       role="alertdialog"
       aria-labelledby="pwa-message"
     >
       <div id="pwa-message" class="mb-3">
-        {{ offlineReady ? 'App ready to work offline' : 'New content available, click the reload button to update.' }}
+        App ready to work offline
       </div>
-      <button
-        v-if="needRefresh"
-        type="button"
-        class="pwa-refresh"
-        @click="updateServiceWorker?.()"
-      >
-        Reload
-      </button>
       <button
         type="button"
         class="pwa-cancel"
@@ -63,27 +46,27 @@ onBeforeMount(async () => {
 </template>
 
 <style>
-.pwa-toast {
-  position: fixed;
-  right: 0;
-  bottom: 0;
-  margin: 16px;
-  padding: 12px;
-  border: 1px solid #8885;
-  border-radius: 4px;
-  z-index: 100;
-  text-align: left;
-  box-shadow: 3px 4px 5px 0 #8885;
-  background-color: white;
-}
-.pwa-toast #pwa-message {
-  margin-bottom: 8px;
-}
-.pwa-toast button {
-  border: 1px solid #8885;
-  outline: none;
-  margin-right: 5px;
-  border-radius: 2px;
-  padding: 3px 10px;
-}
+    .pwa-toast {
+        position: fixed;
+        right: 0;
+        bottom: 0;
+        margin: 16px;
+        padding: 12px;
+        border: 1px solid #8885;
+        border-radius: 4px;
+        z-index: 100;
+        text-align: left;
+        box-shadow: 3px 4px 5px 0 #8885;
+        background-color: white;
+    }
+    .pwa-toast #pwa-message {
+        margin-bottom: 8px;
+    }
+    .pwa-toast button {
+        border: 1px solid #8885;
+        outline: none;
+        margin-right: 5px;
+        border-radius: 2px;
+        padding: 3px 10px;
+    }
 </style>
